2d272be394f1335da0d33c645f590e7cd2b5b327
Podad <79634747+Podad@users.noreply.github.com>
2022-03-14 08:44:26 +0100

    Mise a jour bug

---
+++ pierre-feuille-ciseau.py
@@ -0,0 +1,31 @@
+from asyncio.windows_events import PipeServer
+import math
+import time
+import random
+
+# Pierre Papier Ciseau
+
+pierre = "pierre"
+papier = "papier"
+ciseau = "ciseau"
+a = random.randint(0, 2)
+g = True
+user = input("Enter")
+b = (papier, pierre, ciseau)
+c = b[a]
+d = True
+
+print(c)
+
+
+while d == True:
+
+    if c == "papier":
+        if user == c:
+            print("papier")
+    elif c == "pierre":
+        if user == c:
+            print("pierre")
+    elif c == "ciseau":
+        if user == c:
+            print("ciseau")
