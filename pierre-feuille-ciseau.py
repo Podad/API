abf1ba836740af5a0cf0dcf97ea2b8f0837da5b1
Podad <79634747+Podad@users.noreply.github.com>
2022-03-28 14:35:19 +0200

    adzefgrhtjyku

--- pierre-feuille-ciseau.py
+++ pierre-feuille-ciseau.py
@@ -9,22 +9,11 @@ papier = "papier"
 ciseau = "ciseau"
 a = random.randint(0, 2)
 g = True
-user = input("Enter")
+user = input("Enter ")
 b = (papier, pierre, ciseau)
 c = b[a]
 d = True
 
 print(c)
-
-
-while d == True:
-
-    if c == "papier":
-        if user == c:
-            print("papier")
-    elif c == "pierre":
-        if user == c:
-            print("pierre")
-    elif c == "ciseau":
-        if user == c:
-            print("ciseau")
+time.sleep(1)
+print(user)
